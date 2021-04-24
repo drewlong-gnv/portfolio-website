52a4ea8ab58a2c20d20c50c251b7cddaf6cca274
drewlong-gnv <72838369+drewlong-gnv@users.noreply.github.com>
2021-04-23 21:41:36 -0400

    Update all files dramatically after over a week

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Hey, you found Drew Long's Portfolio!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'right-nav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'right-nav';
+  }
+}
